0599c90545b4d440ab4fd081eef154a3f576ac85
Leschonander <schonanderl@gwu.edu>
2021-10-14 00:02:55 -0400

    first chart added

--- pages/committee/[slug].tsx
+++ pages/committee/[slug].tsx
@@ -2,6 +2,8 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import MaterialTable from 'material-table';
 import * as d3 from 'd3';
+import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, XYChart, Tooltip } from '@visx/xychart';
+import Grid from '@material-ui/core/Grid';
 import { useRouter } from 'next/router';
 
 import React from 'react';
@@ -26,26 +28,102 @@ export default function HearingDashboard() {
   const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const router = useRouter();
-  const { pid } = router.query;
+  const committee = router.query.slug;
+  const date_format = d3.timeParse('%m/%d/%y');
 
   React.useEffect(() => {
     d3.csv(
       'https://raw.githubusercontent.com/Leschonander/SenateVideoScraper/master/SenateVideoFiles/MasterFile.csv'
-    ).then((d) => {
-      console.log(d);
-      setData(d);
+    ).then((data) => {
+      data = data.filter((d) => d.Committee === 'Enviroment');
+      data = data.map((d) => ({ ...d, Year: date_format(d['Date']).getFullYear() }));
+      console.log(data);
+      setData(data);
       setLoading(false);
     });
     return () => undefined;
   }, []);
 
+  const hearingCount = data.length;
+
+  const rollup = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => d.Year
+  );
+  const hearingsPerYear = Array.from(rollup, ([key, value]) => ({ key, value })).reverse();
+  console.log(hearingsPerYear);
+  const accessors = {
+    xAccessor: (d) => d.key,
+    yAccessor: (d) => d.value,
+  };
+  // <AnimatedLineSeries dataKey="Line 1" data={hearingsPerYear} {...accessors} />
+
   return (
     <div className={classes.root}>
       <Typography gutterBottom variant="h4" align="center">
-        Welcome back
+        {committee}
       </Typography>
       {loading && <div>Senate Committee Hearing data loading...</div>}
-      {!loading && <div>Data loaded!</div>}
+      {!loading && (
+        <div>
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <Typography gutterBottom variant="h6" align="center">
+                Total Hearings
+                <br />
+                {hearingCount}
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography gutterBottom variant="h6" align="center">
+                <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+                  <AnimatedAxis orientation="bottom" />
+                  <AnimatedAxis orientation="left" />
+                  <AnimatedLineSeries dataKey="Line 1" data={hearingsPerYear} {...accessors} />
+                  <Tooltip
+                    snapTooltipToDatumX
+                    snapTooltipToDatumY
+                    showVerticalCrosshair
+                    showSeriesGlyphs
+                    renderTooltip={({ tooltipData, colorScale }) => (
+                      <div>
+                        <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
+                          {tooltipData.nearestDatum.key}
+                        </div>
+                        {accessors.xAccessor(tooltipData.nearestDatum.datum)}
+                        {', '}
+                        {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+                      </div>
+                    )}
+                  />
+                </XYChart>
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <MaterialTable
+                columns={[
+                  { title: 'Date', field: 'Date' },
+                  { title: 'Time', field: 'Time' },
+                  { title: 'URL', field: 'URL' },
+                  { title: 'Title', field: 'Title' },
+                  { title: 'Location', field: 'Location' },
+                  { title: 'Committee', field: 'Committee' },
+                  { title: 'Video Url', field: 'video_url' },
+                ]}
+                data={data}
+                title="Senate Committee Hearings"
+                options={{
+                  exportAllData: true,
+                  exportButton: {
+                    csv: true,
+                  },
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      )}
     </div>
   );
 }
