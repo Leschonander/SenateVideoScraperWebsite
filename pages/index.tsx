8f3b2339e93ed93b232e9e9ced699f7defaff4f7
Leschonander <schonanderl@gwu.edu>
2021-10-29 21:43:23 -0400

    year chart for main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import MaterialTable from 'material-table';
 import * as d3 from 'd3';
+import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, XYChart, Tooltip } from '@visx/xychart';
+import Grid from '@material-ui/core/Grid';
 
 import React from 'react';
 
@@ -31,26 +33,72 @@ export default function HearingDashboard() {
     return () => undefined;
   }, []);
 
+  let counts = data.reduce((p, d) => {
+    let year = new Date(d.Date).getFullYear();
+    if (!p.hasOwnProperty(year)) {
+      if (year !== 342) {
+        p[year] = 0;
+      }
+    }
+    if (year !== 342) {
+      p[year]++;
+    }
+    delete p[NaN];
+    return p;
+  }, {});
+
+  counts = Object.keys(datum).map((key: any) => ({ key: key, value: datum[key] }));
+  const accessors = {
+    xAccessor: (d) => d.key,
+    yAccessor: (d) => d.value,
+  };
+
   return (
     <div className={classes.root}>
-      <div>
-        <p>
-          There have been <span className={classes.colorSpan}>{loading === false ? data.length : '?'}</span> hearings
-          collected so far.
-        </p>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <p>
+            There have been <span className={classes.colorSpan}>{loading === false ? data.length : '?'}</span> hearings
+            collected so far.
+          </p>
 
-        <p>
-          This service exists because surprisingly, the Senate does not have a central repository for the hearings. In
-          response to this, I built up a scraper to scrape said hearings and aggergate this data. You can find this{' '}
-          <a href="https://github.com/Leschonander/SenateVideoScraper">here</a>. There you can look at the code, see the
-          various csv's for the a given a committee.
-        </p>
+          <p>
+            This service exists because surprisingly, the Senate does not have a central repository for the hearings. In
+            response to this, I built up a scraper to scrape said hearings and aggergate this data. You can find this{' '}
+            <a href="https://github.com/Leschonander/SenateVideoScraper">here</a>. There you can look at the code, see
+            the various csv's for the a given a committee.
+          </p>
 
-        <p>
-          To actually use this service, you can use the search bar contained in the table to search for a specific
-          hearing. You can also go to the bottom of the page and flip between pages as well.
-        </p>
-      </div>
+          <p>
+            To actually use this service, you can use the search bar contained in the table to search for a specific
+            hearing. You can also go to the bottom of the page and flip between pages as well.
+          </p>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography gutterBottom variant="h6" align="center">
+            Hearings per Year
+          </Typography>
+          <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+            <AnimatedAxis orientation="bottom" />
+            <AnimatedAxis orientation="left" />
+            <AnimatedLineSeries dataKey="Hearings per year" data={counts} {...accessors} />
+            <Tooltip
+              snapTooltipToDatumX
+              snapTooltipToDatumY
+              showVerticalCrosshair
+              showSeriesGlyphs
+              renderTooltip={({ tooltipData, colorScale }) => (
+                <div>
+                  <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>{tooltipData.nearestDatum.key}</div>
+                  {accessors.xAccessor(tooltipData.nearestDatum.datum)}
+                  {', '}
+                  {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+                </div>
+              )}
+            />
+          </XYChart>
+        </Grid>
+      </Grid>
       {loading && <div>Senate Committee Hearing data loading...</div>}
       {!loading && (
         <div>
