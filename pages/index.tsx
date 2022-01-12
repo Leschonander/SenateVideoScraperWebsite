756b0f78d6c6d854b3539852d6e08d488d167368
Leschonander <schonanderl@gwu.edu>
2022-01-12 16:26:26 -0500

    ts ignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,22 +28,39 @@ import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const tableIcons = {
+  // @ts-ignore
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  // @ts-ignore
   Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  // @ts-ignore
   Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  // @ts-ignore
   Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  // @ts-ignore
   DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  // @ts-ignore
   Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  // @ts-ignore
   Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  // @ts-ignore
   Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  // @ts-ignore
   FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  // @ts-ignore
   LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  // @ts-ignore
   NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  // @ts-ignore
   PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  // @ts-ignore
   ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  // @ts-ignore
   Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  // @ts-ignore
   SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  // @ts-ignore
   ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  // @ts-ignore
   ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
 };
 
