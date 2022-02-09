c6c6695daedd48bc38060c32a975e322c30cceba
Leschonander <schonanderl@gwu.edu>
2022-02-09 13:00:33 -0500

    witness page upgrade

---
+++ pages/witnesses/index.js
@@ -0,0 +1,105 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import MaterialTable from 'material-table';
+import * as d3 from 'd3';
+import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, XYChart, Tooltip } from '@visx/xychart';
+import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+
+import React from 'react';
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: { paddingTop: theme.spacing(2) },
+    colorSpan: {
+      textDecorationLine: 'underline',
+    },
+  })
+);
+
+export default function WitnessDashboard() {
+  const classes = useStyles();
+  const url = 'https://raw.githubusercontent.com/Leschonander/SenateVideoScraper/witnessList/wit_count.csv';
+
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    d3.csv(url).then((d) => {
+      setData(d);
+      setLoading(false);
+    });
+    return () => undefined;
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <p>Tracking who speaks to congress</p>
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          {loading && <div>Senate Committee Hearing data witness loading...</div>}
+          {!loading && (
+            <div>
+              <MaterialTable
+                icons={tableIcons}
+                columns={[
+                  { title: 'ID', field: 'id' },
+                  { title: 'Witnesses', field: 'Witnesses' },
+                  { title: 'Count', field: 'count' },
+                ]}
+                data={data}
+                title="Senate Committee Hearings"
+                options={{
+                  exportAllData: true,
+                  exportButton: {
+                    csv: true,
+                  },
+                }}
+              />
+            </div>
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
