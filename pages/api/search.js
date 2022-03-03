22246df6a783de15817fd7cf6106a10efb6db338
Leschonander <schonanderl@gwu.edu>
2022-03-03 13:46:30 -0500

    fix to search api

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,8 +1,26 @@
-import { connectToElasticsearch } from '../../lib/elasticsearch';
+import { Client } from '@elastic/elasticsearch';
+//connect to Elasticsearch
+export async function connectToElasticsearch() {
+  const ESS_CLOUD_ID = process.env.cloud_id;
+  const ESS_CLOUD_USERNAME = process.env.user;
+  const ESS_CLOUD_PASSWORD = process.env.password;
 
+  if (!ESS_CLOUD_ID || !ESS_CLOUD_USERNAME || !ESS_CLOUD_PASSWORD) {
+    return 'ERR_ENV_NOT_DEFINED';
+  }
+
+  return new Client({
+    cloud: {
+      id: ESS_CLOUD_ID,
+    },
+    auth: {
+      username: ESS_CLOUD_USERNAME,
+      password: ESS_CLOUD_PASSWORD,
+    },
+  });
+}
 export default async function search(req, res) {
   const client = await connectToElasticsearch();
-  const query = req.body;
 
   const result = await client.search({
     index: 'first-view',
@@ -16,6 +34,6 @@ export default async function search(req, res) {
     },
   });
 
-  let data = result['hits']['hits'];
+  let data = result['body']['hits']['hits'];
   res.status(200).json({ data: data });
 }
